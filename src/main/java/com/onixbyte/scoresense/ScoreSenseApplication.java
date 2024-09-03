d0272baf8780b4f8b107ae772a873830f56675c4
zihluwang <really@zihlu.wang>
2024-09-03 11:53:56 +0800

    feat: 项目初始化

---
+++ src/main/java/com/onixbyte/scoresense/ScoreSenseApplication.java
@@ -0,0 +1,13 @@
+package com.onixbyte.scoresense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreSenseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreSenseServerApplication.class, args);
+    }
+
+}

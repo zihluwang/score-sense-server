ad183ff4f878667d0c4614445a94ccf2d462e7c5
zihluwang <really@zihlu.wang>
2024-09-24 09:34:07 +0800

    docs: 完善文档

--- src/main/java/com/ronglankj/scoresense/controller/ExamController.java
+++ src/main/java/com/ronglankj/scoresense/controller/ExamController.java
@@ -3,6 +3,7 @@ package com.ronglankj.scoresense.controller;
 import com.mybatisflex.core.paginate.Page;
 import com.ronglankj.scoresense.entity.Exam;
 import com.ronglankj.scoresense.entity.ExamType;
+import com.ronglankj.scoresense.exception.BaseBizException;
 import com.ronglankj.scoresense.model.request.CreateExamRequest;
 import com.ronglankj.scoresense.model.request.CreateExamTypeRequest;
 import com.ronglankj.scoresense.model.request.UpdateExamTypeRequest;
@@ -27,18 +28,29 @@ public class ExamController {
         this.examService = examService;
     }
 
+    /**
+     * 获取分页考试列表。
+     *
+     * @param currentPage 当前页码
+     * @param pageSize    页面大小
+     * @return 考试列表分页数据
+     */
     @GetMapping("/")
     public Page<Exam> getExams(@RequestParam(required = false, defaultValue = "1") Integer currentPage,
                                @RequestParam(required = false, defaultValue = "10") Integer pageSize) {
         return examService.getExamPage(currentPage, pageSize);
     }
 
+    /**
+     * 创建考试。
+     *
+     * @param request 创建考试请求
+     * @return 创建的考试信息
+     */
     @PostMapping("/")
-    public ResponseEntity<Void> createExam(@ModelAttribute CreateExamRequest request) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(null);
+    public Exam createExam(@ModelAttribute CreateExamRequest request) {
+        throw new BaseBizException(HttpStatus.SERVICE_UNAVAILABLE, "接口暂未开放");
     }
-
 
 
     /**
@@ -72,16 +84,34 @@ public class ExamController {
         return examService.getExamTypes(currentPage, pageSize, name);
     }
 
+    /**
+     * 创建考试类型。
+     *
+     * @param request 创建考试类型请求
+     * @return 创建的考试类型
+     */
     @PostMapping("/types")
     public ExamType createExamType(@RequestBody CreateExamTypeRequest request) {
         return examService.createExamType(request.name());
     }
 
+    /**
+     * 修改考试类型。
+     *
+     * @param request 修改考试类型请求
+     * @return 被修改后的考试类型
+     */
     @PatchMapping("/types")
     public ExamType updateExamType(@RequestBody UpdateExamTypeRequest request) {
         return examService.updateExamType(request);
     }
 
+    /**
+     * 删除考试类型。
+     *
+     * @param typeId 需要被删除的考试类型 ID
+     * @return 操作的响应
+     */
     @DeleteMapping("/types/{typeId}")
     public ActionResponse deleteExamType(@PathVariable Integer typeId) {
         examService.deleteExamType(typeId);

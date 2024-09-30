0791694ea9205977c73ea8c54ee15f03e18e828f
zihluwang <really@zihlu.wang>
2024-09-30 16:49:28 +0800

    feat: 完成附件相关接口

--- src/main/java/com/ahgtgk/scoresense/controller/CommonController.java
+++ src/main/java/com/ahgtgk/scoresense/controller/CommonController.java
@@ -3,10 +3,12 @@ package com.ahgtgk.scoresense.controller;
 import com.ahgtgk.scoresense.model.request.ShareQrcodeRequest;
 import com.ahgtgk.scoresense.service.WechatService;
 import com.ahgtgk.scoresense.view.AttachmentView;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CommonController {
 
--- src/main/java/com/ahgtgk/scoresense/controller/DivisionController.java
+++ src/main/java/com/ahgtgk/scoresense/controller/DivisionController.java
@@ -2,6 +2,7 @@ package com.ahgtgk.scoresense.controller;
 
 import com.ahgtgk.scoresense.model.response.ProvinceResponse;
 import com.ahgtgk.scoresense.service.DivisionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/divisions")
 public class DivisionController {
